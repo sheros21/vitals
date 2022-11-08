8d5a9e80fd7e7439273755a60f83ff776a281b62
akumar282 <abhi.kumar7567@gmail.com>
2022-11-08 14:15:02 -0800

    ayo pierre wanna come out here

---
+++ src/backend/main.ts
@@ -0,0 +1,15 @@
+type GenreWordPoints = {
+  genre: string
+  keyword: string
+  points: number
+  book?: string
+  firstPVal?: number
+}
+
+const tteest : GenreWordPoints = { 
+  genre: 'test',
+  keyword: "done",
+  points: 1,
+  book: "erg",
+  firstPVal: 235423
+}
