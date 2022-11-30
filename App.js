7f9aa99a354d3e4609fb10cc50cb366ca1e4a39d
unknown <rosie.tianshen@gmail.com>
2022-11-29 16:27:15 -0800

    Added dependencies for material core, routing and react scripts

--- App.js
+++ App.js
@@ -2,11 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import { Button } from "@react-native-material/core";
+import { TextInput } from "@react-native-material/core";
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vital Tracker</Text>
       <StatusBar style="auto" />
+
+      <TextInput variant="outlined" label="Username" style={{ margin: 2 }} />
+      <TextInput variant="outlined" label="Password" style={{ margin: 2 }} />
+      <Button title="login" style={{ alignSelf: "center", marginTop: 2 }}/>
+      <Button title="create account" style={{ alignSelf: "center", marginTop: 4 }}/>
+
+
     </View>
   );
 }
